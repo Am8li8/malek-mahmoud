1aa3e294b15923bcc3bc6ec391047179d91e59a8
Malek Mahmoud <mane1234567855@gmail.com>
2025-09-20 02:03:13 +0300

    Add files via upload

--- pro.js
+++ pro.js
@@ -8,6 +8,35 @@ function hideSidebar() {
   if (sidebar) sidebar.classList.remove('show');
 }
 
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+// حركة الماوس
+window.addEventListener("mousemove", function(e){
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+
+  cursorOutline.animate({
+    left: `${posX}px`,
+    top: `${posY}px`
+  }, { duration: 500, fill: "forwards" });
+});
+
+// تغيير الشكل عند المرور على اللينك
+document.querySelectorAll("a").forEach(link => {
+  link.addEventListener("mouseenter", () => {
+    cursorDot.classList.add("active");
+    cursorOutline.classList.add("active");
+  });
+  link.addEventListener("mouseleave", () => {
+    cursorDot.classList.remove("active");
+    cursorOutline.classList.remove("active");
+  });
+});
+
 const projects = [
   {
     id: 1,
